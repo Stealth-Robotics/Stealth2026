bb3cb7b58689c10ae6c2460e60036b1b1f16709a
GreenMachineStudios <116412610+GreenMachineStudios@users.noreply.github.com>
2026-02-06 17:16:55 -0800

    whoa (it's all coming together)

--- src/main/java/frc/robot/RobotSystem.java
+++ src/main/java/frc/robot/RobotSystem.java
@@ -1,9 +1,23 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShootingSuperstructure;
+import frc.robot.subsystems.TransferSubsystem;
 
 public class RobotSystem extends SubsystemBase {
+    private final IntakeSubsystem intake;
+    private final TransferSubsystem transfer;
+    private final ShootingSuperstructure shooter;
+
     public RobotSystem() {
+        intake = new IntakeSubsystem();
+        transfer = new TransferSubsystem();
+        shooter = new ShootingSuperstructure();
+    }
+
+    @Override
+    public void periodic() {
         
     }
 }
--- src/main/java/frc/robot/subsystems/TransferSubsystem.java
+++ src/main/java/frc/robot/subsystems/TransferSubsystem.java
@@ -2,19 +2,59 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import dev.doglog.DogLog;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TransferSubsystem extends SubsystemBase {
+    private final TalonFX spindexerMotor;
     private final TalonFX feederMotor;
+
+    private final TalonFXConfiguration spindexerConfig = new TalonFXConfiguration();
     private final TalonFXConfiguration feederConfig = new TalonFXConfiguration();
 
+    //TODO: Tune values for optimal behavior
+    private final double SPINNING_SPEED = 1;
+    private final double FEEDING_SPEED = 1;
+
     //TODO: Find correct CAN IDs
+    private final int SPINDEXER_MOTOR_ID = 0;
     private final int FEEDER_MOTOR_ID = 0;
 
     public TransferSubsystem() {
+        spindexerMotor = new TalonFX(SPINDEXER_MOTOR_ID);
         feederMotor = new TalonFX(FEEDER_MOTOR_ID);
 
+        spindexerConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        spindexerConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+
+        feederConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        feederConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+
+        spindexerMotor.getConfigurator().apply(spindexerConfig);
         feederMotor.getConfigurator().apply(feederConfig);
+    }
+
+    public Command spin() {
+        return runOnce(() -> spindexerMotor.set(SPINNING_SPEED));
+    }
+
+    public Command stopSpin() {
+        return runOnce(() -> spindexerMotor.set(0));
+    }
+
+    public Command feed() {
+        return runOnce(() -> feederMotor.set(FEEDING_SPEED));
+    }
+
+    public Command stopFeed() {
+        return runOnce(() -> feederMotor.set(0));
+    }
+
+    @Override
+    public void periodic() {
     }
 }
