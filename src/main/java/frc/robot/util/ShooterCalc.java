644da7edad85f08196b361197cc2658444af7c1d
Ian N <littlepenguin24@outlook.com>
2026-02-11 06:25:02 -0800

    valid check

--- src/main/java/frc/robot/util/ShooterCalc.java
+++ src/main/java/frc/robot/util/ShooterCalc.java
@@ -5,6 +5,13 @@ import edu.wpi.first.math.geometry.Translation3d;
 public class ShooterCalc {
     private static final double g = 32.2; // Gravitational constant - g = 9.8 m/s^2 = 32.2 ft/s^2
 
+    // Limits on accurate shooting
+    //TODO: TUNE
+    private static final double TURRET_MAX_ANGLE = 90;
+    private static final double TURRET_MIN_ANGLE = 0;
+    private static final double MAX_VELO = 20;
+    private static final double MAX_ACCEL = 4;
+
     //TODO: input wheel diameter
     private static final double SHOOTER_WHEEL_CIRCUMFERENCE = 3 * Math.PI; // Circumference of the shooter wheel, in inches, for RPM calc
 
@@ -15,6 +22,7 @@ public class ShooterCalc {
     private static double shootVelocity = 0;
     private static double turretAngle = 0;
     private static double hoodAngle = 0;
+    private static boolean isValid = true;
 
     // Updates shootVelocity, turretAngle, and hoodAngle                                                         Coordinate System:                      [Ceiling]
     // ballPose - Position of the ball in relation to the field (bot pos + turret pos on robot)                           [Opposing side]                    ^ (+Y)
@@ -37,19 +45,27 @@ public class ShooterCalc {
         double t = Math.sqrt(2 * (height - ballPose.getY()) / g) + Math.sqrt(2 * (height - goalPose.getY()) / g);
 
         // Calculated launch velocity vector - will land in goal at time t
-        Vector3d ballVelo = new Vector3d(
+        Translation3d ballVelo = new Translation3d(
                 ANTIDRAG_COEFFICIENT * (goalPose.getX() - ballPoseFuture.getX()) / t - botVeloFuture.getX(),
                 (goalPose.getY() - ballPoseFuture.getY()) / t - g * t / 2,
                 ANTIDRAG_COEFFICIENT * (goalPose.getZ() - ballPoseFuture.getZ()) / t - botVeloFuture.getZ()
         );
 
         // Update values to match velocity vector
-        shootVelocity = ballVelo.magnitude();
-        turretAngle = Math.atan(ballVelo.z / ballVelo.x);
-        hoodAngle = Math.atan(ballVelo.y / ballVelo.x * Math.cos(turretAngle));
+        shootVelocity = ballVelo.getNorm();
+        turretAngle = Math.atan(ballVelo.getZ() / ballVelo.getX());
+        hoodAngle = Math.atan(ballVelo.getY() / ballVelo.getX() * Math.cos(turretAngle));
 
         // Account for latency in bot rotation by applying bot rotation to turret
         turretAngle -= botRPMCCW / 60 * LATENCY_COMPENSATION;
+
+        // Check whether optimal conditions are met for a successful shot
+        isValid = (botAccel.getNorm() < MAX_ACCEL && botVeloFuture.getNorm() < MAX_VELO && angleInRange(turretAngle, TURRET_MIN_ANGLE, TURRET_MAX_ANGLE));
+    }
+    private static boolean angleInRange(double angle, double min, double max) {
+        if(max > min) return (angle < max && angle > min);
+        else if (angle < min) return (angle < max && angle + 2 * Math.PI > min);
+        return (angle < max + 2 * Math.PI && angle > min);
     }
 
     public double getShootVelocity() { return shootVelocity; }
@@ -57,4 +73,5 @@ public class ShooterCalc {
     public double getShootRPM() { return shootVelocity * 720 / SHOOTER_WHEEL_CIRCUMFERENCE; }
     public double getTurretAngle() { return turretAngle; }
     public double getHoodAngle() { return hoodAngle; }
+    public boolean validShot() { return isValid; }
 }
