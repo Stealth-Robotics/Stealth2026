4edf7fe480ee39426cdd8d46c935fdaa1ed060d2
Ian N <littlepenguin24@outlook.com>
2026-02-10 23:13:29 -0800

    I changed like 3 lines but its good now (maybe)

--- src/main/java/frc/robot/util/ShooterCalc.java
+++ src/main/java/frc/robot/util/ShooterCalc.java
@@ -1,35 +1,60 @@
 package frc.robot.util;
 
+import edu.wpi.first.math.geometry.Translation3d;
+
 public class ShooterCalc {
-    private static final double g = -32.2; // g = 32.2 ft/s^2
+    private static final double g = 32.2; // Gravitational constant - g = 9.8 m/s^2 = 32.2 ft/s^2
+
+    //TODO: input wheel diameter
+    private static final double SHOOTER_WHEEL_CIRCUMFERENCE = 3 * Math.PI; // Circumference of the shooter wheel, in inches, for RPM calc
+
+    //TODO: TUNE
     private static final double LATENCY_COMPENSATION = 0.040; // Estimated 40 ms of latency
+    private static final double ANTIDRAG_COEFFICIENT = 1.3; // Coefficient multiplied by horizontal shot velocity to compensate for drag
 
     private static double shootVelocity = 0;
     private static double turretAngle = 0;
     private static double hoodAngle = 0;
 
-    public static void update(Vector3d ballPose, Vector3d botVelo, Vector3d goalPose, double t) {
+    // Updates shootVelocity, turretAngle, and hoodAngle                                                         Coordinate System:                      [Ceiling]
+    // ballPose - Position of the ball in relation to the field (bot pos + turret pos on robot)                           [Opposing side]                    ^ (+Y)
+    // botVelo - bot velocity in feet/second                                                                                     ^ (+Z)                      |
+    // botAccel - bot acceleration in feet/second^2 for latency compensation                                                     |                           |
+    // goalPose - Position of the goal in relation to the field (z forward, x right, origin doesn't matter)      [left side] <-[Bot]-> [right side]          V (-Y)
+    // botRPMCCW - Rotational velocity of the bot in Counterclockwise Rotations per Minute                           (-X)                  (+X)           [Floor]
+    // height - target maxHeight of the shot, in feet (~13 for shooting, ~5 for passing)
+    public static void update(Translation3d ballPose, Translation3d botVelo, Translation3d botAccel, Translation3d goalPose, double botRPMCCW, double height) {
 
-        // Account for latency by adding a bit to ball position
-        Vector3d ballPoseFuture = new Vector3d(ballPose.x + botVelo.x * LATENCY_COMPENSATION, ballPose.y,
-                ballPose.z + botVelo.z * LATENCY_COMPENSATION);
+        // Account for latency by adding a bit to ball position | NOTE: does not account for acceleration
+        Translation3d ballPoseFuture = new Translation3d(ballPose.getX() + botVelo.getX() * LATENCY_COMPENSATION, ballPose.getY(),
+                ballPose.getZ() + botVelo.getZ() * LATENCY_COMPENSATION);
+
+        // Account for latency by adding a bit to ball velocity | NOTE: if acceleration is too high, shots may be inaccurate
+        Translation3d botVeloFuture = new Translation3d(botVelo.getX() + botAccel.getX() * LATENCY_COMPENSATION, botVelo.getY(),
+                botVelo.getZ() + botAccel.getZ() * LATENCY_COMPENSATION);
+
+        // t is calculated to be the time for the ball to reach desired height, and return to goal height, under vacuum conditions
+        double t = Math.sqrt(2 * (height - ballPose.getY()) / g) + Math.sqrt(2 * (height - goalPose.getY()) / g);
 
         // Calculated launch velocity vector - will land in goal at time t
         Vector3d ballVelo = new Vector3d(
-                (goalPose.x - ballPoseFuture.x) / t - botVelo.x,
-                (goalPose.y - ballPoseFuture.y) / t + g * t / 2,
-                (goalPose.z - ballPoseFuture.z) / t - botVelo.z
+                ANTIDRAG_COEFFICIENT * (goalPose.getX() - ballPoseFuture.getX()) / t - botVeloFuture.getX(),
+                (goalPose.getY() - ballPoseFuture.getY()) / t - g * t / 2,
+                ANTIDRAG_COEFFICIENT * (goalPose.getZ() - ballPoseFuture.getZ()) / t - botVeloFuture.getZ()
         );
 
         // Update values to match velocity vector
         shootVelocity = ballVelo.magnitude();
         turretAngle = Math.atan(ballVelo.z / ballVelo.x);
         hoodAngle = Math.atan(ballVelo.y / ballVelo.x * Math.cos(turretAngle));
+
+        // Account for latency in bot rotation by applying bot rotation to turret
+        turretAngle -= botRPMCCW / 60 * LATENCY_COMPENSATION;
     }
 
     public double getShootVelocity() { return shootVelocity; }
-    // 2 is wheel radius (inches)
-    public double getShootRPM() { return shootVelocity * 30 / (Math.PI * 2 / 12); }
+    // RPM = [velocity]ft/s * 12 in/ft * 60 s/min * 1 rotation/[Circumference] in = 720/[Circumference]
+    public double getShootRPM() { return shootVelocity * 720 / SHOOTER_WHEEL_CIRCUMFERENCE; }
     public double getTurretAngle() { return turretAngle; }
     public double getHoodAngle() { return hoodAngle; }
 }
