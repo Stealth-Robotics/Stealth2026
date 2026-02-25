ee7a125f957a40518563417dbd91f815d37006b8
GreenMachineStudios <116412610+GreenMachineStudios@users.noreply.github.com>
2026-02-24 20:16:54 -0800

    updated LL estimation code (always use blue estimate_

--- src/main/java/frc/robot/RobotSystem.java
+++ src/main/java/frc/robot/RobotSystem.java
@@ -4,6 +4,7 @@ import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
 import dev.doglog.DogLog;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -99,21 +100,15 @@ public class RobotSystem extends SubsystemBase {
     }
 
     private void updateOdometryEstimateWithLimelight() {
-        LimelightHelpers.SetRobotOrientation("limelight-robot", drive.getPigeon2().getRotation2d().getDegrees(), 0, 0, 0, 0, 0);
+        double imuAngle = drive.getPigeon2().getRotation2d().getDegrees();
+        LimelightHelpers.SetRobotOrientation("limelight-robot", imuAngle, 0, 0, 0, 0, 0);
 
         double robotAngularVelocity = drive.getFieldRelativeVelocity().omegaRadiansPerSecond;
-        if (Math.abs(robotAngularVelocity) < 2 * Math.PI) {
-            if (AllianceUtility.getAlliance().equals(Alliance.Blue)) {
-                PoseEstimate poseEstimate = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight-robot");
-                if (poseEstimate != null && poseEstimate.tagCount > 0) {
-                    drive.addVisionMeasurement(poseEstimate.pose, poseEstimate.timestampSeconds);
-                }
-            }
-            else {
-                PoseEstimate poseEstimate = LimelightHelpers.getBotPoseEstimate_wpiRed_MegaTag2("limelight-robot");
-                if (poseEstimate != null && poseEstimate.tagCount > 0) {
-                    drive.addVisionMeasurement(poseEstimate.pose, poseEstimate.timestampSeconds);
-                }
+        if (Math.abs(robotAngularVelocity) < Math.PI) {
+            PoseEstimate poseEstimate = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight-robot");
+            if (poseEstimate != null && poseEstimate.tagCount > 0) {
+                //TODO: Tune standard deviations
+                drive.addVisionMeasurement(poseEstimate.pose, poseEstimate.timestampSeconds, VecBuilder.fill(.7,.7,9999999));
             }
         }
     }
