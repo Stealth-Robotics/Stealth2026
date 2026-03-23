c6f31dd847aceb7f4f81fd8d724090f73d19e8d8
Oliver Stivers <oliverstivers@gmail.com>
2026-03-23 12:09:12 -0700

    added auto in code

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -22,7 +22,7 @@ public class Autos {
 
     public Autos(AutoFactory autoFactory, IntakeSubsystem intake, ShootingSuperstructure shooter) {
         this.autoFactory = autoFactory;
-        
+
         this.intake = intake;
         this.shooter = shooter;
     }
@@ -40,7 +40,7 @@ public class Autos {
     }
 
     private Command stopAgitating() {
-        //Overrides the agitating using requirements
+        // Overrides the agitating using requirements
         return new InstantCommand(() -> intake.stopCommand());
     }
 
@@ -68,19 +68,46 @@ public class Autos {
         path2.atTime("StartShooting2").onTrue(shooter.shoot().alongWith(startAgitating()));
 
         routine.active().onTrue(
-            new SequentialCommandGroup(
-                path.resetOdometry(),
-                path.cmd()
-            )
-        );
+                new SequentialCommandGroup(
+                        path.resetOdometry(),
+                        path.cmd()));
 
         path.done().onTrue(
-            new SequentialCommandGroup(
-                new WaitCommand(4), //Shooting time after first cycle
-                stopShooting(),
-                path2.cmd()
-            )
-        );
+                new SequentialCommandGroup(
+                        new WaitCommand(4), // Shooting time after first cycle
+                        stopShooting(),
+                        path2.cmd()));
+
+        return routine;
+    }
+
+    public AutoRoutine left2_bump() {
+        String pathName = "L2Cycle_bump";
+
+        AutoRoutine routine = autoFactory.newRoutine("routine");
+
+        AutoTrajectory path = routine.trajectory(pathName, 0);
+        path.atTime("StartIntaking").onTrue(deployAndIntake());
+        path.atTime("StopIntaking").onTrue(intake.stopCommand());
+        path.atTime("SpinUp").onTrue(shooter.spinUp(2000));
+        path.atTime("StartShooting").onTrue(shooter.shoot());
+
+        AutoTrajectory path2 = routine.trajectory(pathName, 1);
+        path2.atTime("StartIntaking2").onTrue(deployAndIntake());
+        path2.atTime("StopIntaking2").onTrue(intake.stopCommand());
+        path2.atTime("SpinUp2").onTrue(shooter.spinUp(2000));
+        path2.atTime("StartShooting2").onTrue(shooter.shoot().alongWith(startAgitating()));
+
+        routine.active().onTrue(
+                new SequentialCommandGroup(
+                        path.resetOdometry(),
+                        path.cmd()));
+
+        path.done().onTrue(
+                new SequentialCommandGroup(
+                        new WaitCommand(4), // Shooting time after first cycle
+                        stopShooting(),
+                        path2.cmd()));
 
         return routine;
     }
@@ -97,18 +124,14 @@ public class Autos {
         path.atTime("StopShooting").onTrue(shooter.shoot());
 
         routine.active().onTrue(
-            new SequentialCommandGroup(
-                path.resetOdometry(),
-                path.cmd()
-            )
-        );
+                new SequentialCommandGroup(
+                        path.resetOdometry(),
+                        path.cmd()));
 
         path.done().onTrue(
-            new SequentialCommandGroup(
-                new WaitCommand(5),
-                shooter.shoot().alongWith(intake.agitate().repeatedly()).alongWith(intake.intakeCommand())
-            )
-        );
+                new SequentialCommandGroup(
+                        new WaitCommand(5),
+                        shooter.shoot().alongWith(intake.agitate().repeatedly()).alongWith(intake.intakeCommand())));
 
         return routine;
     }
@@ -118,28 +141,24 @@ public class Autos {
 
         String pathName = "LeftOneCyclePlusDepotSlow";
 
-        AutoTrajectory path = routine.trajectory(pathName,0);
+        AutoTrajectory path = routine.trajectory(pathName, 0);
         path.atTime("StartIntaking").onTrue(intake.intakeCommand().alongWith(intake.deployCommand()));
         path.atTime("StopIntaking").onTrue(intake.stopCommand());
         path.atTime("SpinUp").onTrue(shooter.spinUp(2500));
         path.atTime("StartShooting").onTrue(shooter.shoot());
         path.atTime("StopShooting").onTrue(intake.intakeCommand().alongWith(shooter.shoot()));
 
-        AutoTrajectory path2 = routine.trajectory(pathName,1);
+        AutoTrajectory path2 = routine.trajectory(pathName, 1);
 
         routine.active().onTrue(
-            new SequentialCommandGroup(
-                path.resetOdometry(),
-                path.cmd()
-            )
-        );
+                new SequentialCommandGroup(
+                        path.resetOdometry(),
+                        path.cmd()));
 
         path.done().onTrue(
-            new SequentialCommandGroup(
-                new WaitCommand(1.5),
-                path2.cmd().alongWith(shooter.shoot()).alongWith(intake.agitate().repeatedly())
-            )
-        );
+                new SequentialCommandGroup(
+                        new WaitCommand(1.5),
+                        path2.cmd().alongWith(shooter.shoot()).alongWith(intake.agitate().repeatedly())));
 
         return routine;
     }
@@ -152,64 +171,59 @@ public class Autos {
         AutoTrajectory path = routine.trajectory(pathName, 0);
 
         routine.active().onTrue(
-            new SequentialCommandGroup(
-                path.resetOdometry(),
-                shooter.shoot(),
-                path.cmd()
-            )
-        );
+                new SequentialCommandGroup(
+                        path.resetOdometry(),
+                        shooter.shoot(),
+                        path.cmd()));
 
         return routine;
     }
-    
+
     public AutoRoutine simpleCenterAuto() {
         AutoRoutine routine = autoFactory.newRoutine("simpleCenterAuto");
         String pathName = "SimpleShoot";
-        
-        AutoTrajectory drive = routine.trajectory(pathName,0);
-        AutoTrajectory shoot = routine.trajectory(pathName,1);
-        AutoTrajectory stopspot = routine.trajectory(pathName,2);
+
+        AutoTrajectory drive = routine.trajectory(pathName, 0);
+        AutoTrajectory shoot = routine.trajectory(pathName, 1);
+        AutoTrajectory stopspot = routine.trajectory(pathName, 2);
 
         routine.active().onTrue(
-            Commands.sequence(
-                drive.resetOdometry(),
-                new InstantCommand(() -> shooter.setState(ShooterState.HUB_TRACKING)),
-                drive.cmd().withTimeout(4))
-        );
+                Commands.sequence(
+                        drive.resetOdometry(),
+                        new InstantCommand(() -> shooter.setState(ShooterState.HUB_TRACKING)),
+                        drive.cmd().withTimeout(4)));
 
         drive.done().onTrue(
-            Commands.sequence(
-                shooter.shoot(),
-                new WaitCommand(5)
-            ).andThen(shoot.cmd()).withTimeout(4)
-        );
+                Commands.sequence(
+                        shooter.shoot(),
+                        new WaitCommand(5)).andThen(shoot.cmd()).withTimeout(4));
 
         shoot.done().onTrue(stopspot.cmd());
-        
+
         return routine;
     }
 
-    //Irrelevent until we have a climb
+    // Irrelevent until we have a climb
     // public AutoRoutine centerClimbAuto() {
-    //     AutoRoutine routine = autoFactory.newRoutine("centerClimbAuto");
-    //     String pathName = "CenterPreloadPlusClimb";
+    // AutoRoutine routine = autoFactory.newRoutine("centerClimbAuto");
+    // String pathName = "CenterPreloadPlusClimb";
 
-    //     AutoTrajectory path = routine.trajectory(pathName, 0);
+    // AutoTrajectory path = routine.trajectory(pathName, 0);
 
-    //     path.atTime("DeployClimb").onTrue(climb.reach());
-    //     path.atTime("StartShoot").onTrue(shooter.shoot());
+    // path.atTime("DeployClimb").onTrue(climb.reach());
+    // path.atTime("StartShoot").onTrue(shooter.shoot());
 
-    //     routine.active().onTrue(
-    //         Commands.sequence(
-    //             path.resetOdometry(),
-    //             new InstantCommand(() -> shooter.setState(ShooterState.HUB_TRACKING)),
-    //             shooter.spinUp(1500),
-    //             path.cmd().withTimeout(18)
-    //         )
-    //     );
-    //     path.done().onTrue(
-    //         climb.ascend()
-    //     );
-    //     return routine;
+    // routine.active().onTrue(
+    // Commands.sequence(
+    // path.resetOdometry(),
+    // new InstantCommand(() -> shooter.setState(ShooterState.HUB_TRACKING)),
+    // shooter.spinUp(1500),
+    // path.cmd().withTimeout(18)
+    // )
+    // );
+    // path.done().onTrue(
+    // climb.ascend()
+    // );
+    // return routine;
     // }
 }
