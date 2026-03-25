e682725c99f2febfb429995f2fb9d2ee45f51c50
Siddharth Rao <siddrao.926@gmail.com>
2026-03-24 22:21:27 -0700

    LED Strips Installed

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -1,15 +1,12 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.CANdleConfiguration;
-import com.ctre.phoenix6.controls.ColorFlowAnimation;
+import com.ctre.phoenix6.controls.RainbowAnimation;
 import com.ctre.phoenix6.controls.SolidColor;
 import com.ctre.phoenix6.hardware.CANdle;
-import com.ctre.phoenix6.signals.Enable5VRailValue;
 import com.ctre.phoenix6.signals.LossOfSignalBehaviorValue;
 import com.ctre.phoenix6.signals.RGBWColor;
-import com.ctre.phoenix6.signals.StatusLedWhenActiveValue;
-import com.ctre.phoenix6.signals.VBatOutputModeValue;
-
+import com.ctre.phoenix6.signals.StripTypeValue;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDSubsystem extends SubsystemBase {
@@ -18,15 +15,14 @@ public class LEDSubsystem extends SubsystemBase {
     private final CANdle candle;
     private final CANdleConfiguration candleConfig;
 
-    private final SolidColor hubActiveAnimation = new SolidColor(0, 7)
+    private final SolidColor hubActiveAnimation = new SolidColor(8, 18)
         .withColor(new RGBWColor(12, 199, 93));
 
-    private final SolidColor hubInactiveAnimation = new SolidColor(0, 7)
+    private final SolidColor hubInactiveAnimation = new SolidColor(8, 18)
         .withColor(new RGBWColor(199, 34, 12));
 
-    private final ColorFlowAnimation disabledAnimation = new ColorFlowAnimation(0, 7)
-        .withFrameRate(10)
-        .withColor(new RGBWColor(0, 96, 148));
+    private final RainbowAnimation disabledAnimation = new RainbowAnimation(8, 18)
+        .withFrameRate(20);
 
     private boolean applyAnimation = true;
 
@@ -42,10 +38,8 @@ public class LEDSubsystem extends SubsystemBase {
         candle = new CANdle(CANDLE_ID);
         candleConfig = new CANdleConfiguration();
         
-        candleConfig.LED.BrightnessScalar = 0.1;
-        candleConfig.CANdleFeatures.Enable5VRail = Enable5VRailValue.Disabled;
-        candleConfig.CANdleFeatures.StatusLedWhenActive = StatusLedWhenActiveValue.Enabled;
-        candleConfig.CANdleFeatures.VBatOutputMode = VBatOutputModeValue.Off;
+        candleConfig.LED.BrightnessScalar = 0.5;
+        candleConfig.LED.StripType = StripTypeValue.RGB;
         candleConfig.LED.LossOfSignalBehavior = LossOfSignalBehaviorValue.DisableLEDs;
 
         candle.getConfigurator().apply(candleConfig);
