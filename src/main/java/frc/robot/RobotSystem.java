ccce1dcb9102b4fbc84e98fab6d02cc8d6d4ac13
Ian N <littlepenguin24@outlook.com>
2026-03-31 16:35:12 -0700

    intake logic

--- src/main/java/frc/robot/RobotSystem.java
+++ src/main/java/frc/robot/RobotSystem.java
@@ -21,6 +21,7 @@ import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
@@ -139,7 +140,7 @@ public class RobotSystem extends SubsystemBase {
         pdhNotifier.startPeriodic(0.5);
     }
 
-    public void setIntakeDefaultCommand(DoubleSupplier rollerSpeed, BooleanSupplier deploy, BooleanSupplier retract) {
+    public void setIntakeDefaultCommand(DoubleSupplier rollerSpeed, BooleanSupplier deploy, BooleanSupplier retract, BooleanSupplier agitate) {
         Command intakeDefaultCommand = run(
             () -> {
                 double targetRollerSpeed = rollerSpeed.getAsDouble();
@@ -151,7 +152,10 @@ public class RobotSystem extends SubsystemBase {
                 deployTrigger.onTrue(intake.deployCommand());
 
                 Trigger retractTrigger = new Trigger(retract);
-                retractTrigger.onTrue(intake.retractCommand());
+                retractTrigger.onTrue(new ConditionalCommand(intake.retractCommand(), intake.agitate(), agitate));
+
+                Trigger agitateTrigger = new Trigger(agitate);
+                agitateTrigger.onTrue(intake.agitate());
             }
         );
 
