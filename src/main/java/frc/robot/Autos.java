e83398857054aa0d4a55de25a97e8fa7f390ecd0
Marco <116412610+GreenMachineStudios@users.noreply.github.com>
2026-04-13 19:15:39 -0700

    bleh

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -11,7 +11,6 @@ import frc.robot.lib.BLine.Path;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShootingSuperstructure;
-import frc.robot.util.AutoSide;
 
 public class Autos {
     private FollowPath.Builder pathBuilder;
