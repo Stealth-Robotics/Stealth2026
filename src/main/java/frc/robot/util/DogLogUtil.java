bce6891e7bd0472f01ab2c462f962d1864a477b9
Jeff D <jdamp@wavecable.com>
2026-04-21 14:49:06 -0700

    restore LL logging interval

--- src/main/java/frc/robot/util/DogLogUtil.java
+++ src/main/java/frc/robot/util/DogLogUtil.java
@@ -4,7 +4,7 @@ import dev.doglog.DogLog;
 
 public class DogLogUtil {
     public static final long LOW_PRI_LOGGING_INTERVAL_MS = 250;
-    public static final long LIMELIGHT_LOGGING_INTERVAL = 60;
+    public static final long LIMELIGHT_LOGGING_INTERVAL = 200;
     public static final long HIGH_PRI_STATS_LOGGING_INTERVAL = 40;
     
     //Logs a double to two decimal places
